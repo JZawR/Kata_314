2f54ee1fe109acfb2eaf239908a6dc9a9bfcb4be
JZawR <94790420+JZawR@users.noreply.github.com>
2022-02-18 10:05:06 +0300

    commit 1

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -1,66 +1,66 @@
-package ru.kata.spring.boot_security.demo.controllers;
-
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.services.UserService;
-
-import java.security.Security;
-
-
-@Controller
-@RequestMapping()
-public class UserController {
-    private final UserService userService;
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
-
-    @GetMapping("/")
-    public String toLogin() {
-        return "redirect:/login";
-    }
-
-    @GetMapping("/admin")
-    public String showAllUsers(Model model) {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("admen", user);
-        model.addAttribute("mans", userService.getAllUsers());
-        model.addAttribute("userRoles", userService.getRoles());
-        User emptyUser = new User();
-        model.addAttribute("emptyUser", emptyUser);
-        return "admin";
-    }
-
-    @GetMapping("/user")
-    public String showUserForUser (Model userModel) {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        userModel.addAttribute("man", user);
-        return "user";
-    }
-
-    @PatchMapping("/edit/{id}")
-    public String updateUser(@ModelAttribute("user") User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        userService.updateUser(user);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-        return "redirect:/admin";
-    }
-
-    @PostMapping("/admin/create")
-    public String createUser(@ModelAttribute("emptyUser") User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        userService.addUser(user);
-        return "redirect:/admin";
-    }
-}
+//package ru.kata.spring.boot_security.demo.controllers;
+//
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.*;
+//import ru.kata.spring.boot_security.demo.models.User;
+//import ru.kata.spring.boot_security.demo.services.UserService;
+//
+//import java.security.Security;
+//
+//
+//@Controller
+//@RequestMapping()
+//public class UserController {
+//    private final UserService userService;
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+//
+//    @GetMapping("/")
+//    public String toLogin() {
+//        return "redirect:/login";
+//    }
+//
+//    @GetMapping("/admin")
+//    public String showAllUsers(Model model) {
+//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        model.addAttribute("admen", user);
+//        model.addAttribute("mans", userService.getAllUsers());
+//        model.addAttribute("userRoles", userService.getRoles());
+//        User emptyUser = new User();
+//        model.addAttribute("emptyUser", emptyUser);
+//        return "admin";
+//    }
+//
+//    @GetMapping("/user")
+//    public String showUserForUser (Model userModel) {
+//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        userModel.addAttribute("man", user);
+//        return "user";
+//    }
+//
+//    @PatchMapping("/edit/{id}")
+//    public String updateUser(@ModelAttribute("user") User user) {
+//        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+//        userService.updateUser(user);
+//        return "redirect:/admin";
+//    }
+//
+//    @DeleteMapping("/delete/{id}")
+//    public String deleteUser(@PathVariable Long id) {
+//        userService.deleteUser(id);
+//        return "redirect:/admin";
+//    }
+//
+//    @PostMapping("/admin/create")
+//    public String createUser(@ModelAttribute("emptyUser") User user) {
+//        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+//        userService.addUser(user);
+//        return "redirect:/admin";
+//    }
+//}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserRestController.java
@@ -0,0 +1,76 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+@RestController
+@RequestMapping()
+public class UserRestController {
+    private final UserService userService;
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
+    @GetMapping("/")
+    public String toLogin() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<List<User>> showAllUsers() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        // REST
+        final List<User> usersList = userService.getAllUsers();
+        return usersList != null && !usersList.isEmpty()
+                ? new ResponseEntity<>(usersList, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<User> showUserForUser () {
+        final User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return user != null
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PatchMapping("/edit/{id}")
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        try {
+            userService.updateUser(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        try {
+            userService.deleteUser(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/admin/create")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userService.addUser(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
